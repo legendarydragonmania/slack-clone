876018f320428e23e1993e0941cbf74a6a59fca5
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-17 22:35:51 +0500

    inital commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCLwIAZY7y4o7vv2s4YeJORYCWzzxdtYS4',
+  authDomain: 'slack-clone-f8d4c.firebaseapp.com',
+  projectId: 'slack-clone-f8d4c',
+  storageBucket: 'slack-clone-f8d4c.appspot.com',
+  messagingSenderId: '311070850944',
+  appId: '1:311070850944:web:a217d8ecebd93860094b65',
+}
+
+const app = initializeApp(firebaseConfig)
